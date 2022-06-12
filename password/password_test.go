94f901f590f932498a3940408ff2735cf6ddfe63
Alex Pressley <alexp@computematrix.net>
2022-06-12 04:37:36 -0700

    Add datasource test

--- password/password_test.go
+++ password/password_test.go
@@ -1,3 +1,13 @@
 package password
 
-// TODO: write tests
+import (
+	"testing"
+)
+
+func TestDatasource(t *testing.T) {
+	d := Datasource{config: Config{}}
+	err := d.Configure(nil)
+	if err != nil {
+		t.Fatalf("Failed to configure datasource")
+	}
+}
